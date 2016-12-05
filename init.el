1f2732cf37da9f6c8a1174e6fa6333b591d3c784
Philipp Gabler <phips.gabler@gmail.com>
2016-12-05 18:25:06 +0100

    Refactor everything to use use-package

--- init.el
+++ init.el
@@ -1,101 +1,55 @@
+;; ;; CUSTOMIZE (in extra file)
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+
 ;; ;; GENERAL
-(global-linum-mode 1)
-(global-hl-line-mode 1) ; turn on highlighting current line
-(global-visual-line-mode 1) ; break lines at word boundaries
-(delete-selection-mode 1) ; delete selected text when typing
-(load-library "paren")
+(setq-default inhibit-startup-screen t)
+(global-linum-mode t)
+(setq-default column-number-mode t)
+(global-hl-line-mode t) ; turn on highlighting current line
+;; (global-visual-line-mode t) ; break lines at word boundaries
+
+(setq-default delete-selection-mode 1 ; delete selected text when typing
+              indent-tabs-mode nil
+              tab-width 2)
+
+;; autofill mode
+(setq fill-column 100)
+(add-hook 'text-mode-hook 'turn-on-auto-fill)
+(add-hook 'tex-mode-hook 'turn-on-auto-fill)
 
 
+;; reactivate downcasing
+(put 'downcase-region 'disabled nil)
 
-;; ;; CUSTOMIZE
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(TeX-PDF-mode t)
- '(TeX-command-BibTeX "Biber")
- '(TeX-command-list
-   (quote
-    (("TeX" "%(PDF)%(tex) %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
-      (plain-tex-mode texinfo-mode ams-tex-mode)
-      :help "Run plain TeX")
-     ("LaTeX" "%`%l%(mode)%' %t" TeX-run-TeX nil
-      (latex-mode doctex-mode)
-      :help "Run LaTeX")
-     ("Makeinfo" "makeinfo %t" TeX-run-compile nil
-      (texinfo-mode)
-      :help "Run Makeinfo with Info output")
-     ("Makeinfo HTML" "makeinfo --html %t" TeX-run-compile nil
-      (texinfo-mode)
-      :help "Run Makeinfo with HTML output")
-     ("AmSTeX" "%(PDF)amstex %`%S%(PDFout)%(mode)%' %t" TeX-run-TeX nil
-      (ams-tex-mode)
-      :help "Run AMSTeX")
-     ("ConTeXt" "texexec --once --texutil %(execopts)%t" TeX-run-TeX nil
-      (context-mode)
-      :help "Run ConTeXt once")
-     ("ConTeXt Full" "texexec %(execopts)%t" TeX-run-TeX nil
-      (context-mode)
-      :help "Run ConTeXt until completion")
-     ("BibTeX" "bibtex %s" TeX-run-BibTeX nil t :help "Run BibTeX")
-     ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer")
-     ("Print" "%p" TeX-run-command t t :help "Print the file")
-     ("Queue" "%q" TeX-run-background nil t :help "View the printer queue" :visible TeX-queue-command)
-     ("File" "%(o?)dvips %d -o %f " TeX-run-command t t :help "Generate PostScript file")
-     ("Index" "makeindex %s" TeX-run-command nil t :help "Create index file")
-     ("Check" "lacheck %s" TeX-run-compile nil
-      (latex-mode)
-      :help "Check LaTeX file for correctness")
-     ("Spell" "(TeX-ispell-document \"\")" TeX-run-function nil t :help "Spell-check the document")
-     ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
-     ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")
-     ("Other" "" TeX-run-command t t :help "Run an arbitrary command")
-     ("Biber" "biber %s.bib" TeX-run-BibTeX nil t))))
- '(TeX-source-correlate-method (quote synctex))
- '(TeX-source-correlate-mode nil)
- '(TeX-source-correlate-start-server nil)
- '(TeX-view-program-list (quote (("texworks" "texworks %o"))))
- '(TeX-view-program-selection
-   (quote
-    (((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "texworks")
-     (output-html "xdg-open"))))
- '(column-number-mode t)
- '(custom-safe-themes
-   (quote
-    ("6c57adb4d3da69cfb559e103e555905c9eec48616104e217502d0a372e63dcea" default)))
- '(ess-swv-processor (quote knitr))
- '(fill-column 100)
- '(font-latex-fontify-sectioning (quote color))
- '(font-latex-quotes nil)
- '(haskell-indent-offset 2)
- '(haskell-program-name "ghci")
- '(inhibit-startup-screen t)
- '(package-selected-packages
-   (quote
-    (rainbow-delimiters pandoc-mode web-mode tabbar scala-mode2 multiple-cursors markdown-mode idris-mode haskell-mode ess darkroom color-theme-modern auctex)))
- '(python-python-command "python3")
- '(python-shell-interpreter "ipython3")
- '(safe-local-variable-values
-   (quote
-    ((TeX-master . "../document")
-     (TeX-master . "document")
-     (TeX-command-extra-options . "-shell-escape")
-     (TeX-engine . xelatex))))
- '(tex-bibtex-command "Biber"))
+;; various key bindings
+(global-set-key (kbd "C-x a r") 'align-regexp)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; always ask the same way
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;; define location of backups and save-places
+(setq 
+    backup-directory-alist `(("." . "~/.emacs.cache/emacssaves"))
+    kept-new-versions 1
+    kept-old-versions 1)
+
+
+;; ;; BUILTIN MODES
+;; dired
+(put 'dired-find-alternate-file 'disabled nil)
+(add-hook 'dired-mode-hook
+          (lambda ()
+            (define-key dired-mode-map (kbd "^")
+              (lambda () (interactive) (find-alternate-file "..")))))
+
+;; lisp mode
+(add-hook 'lisp-mode-hook '(lambda ()
+                             (local-set-key (kbd "RET") 'newline-and-indent))) 
+
 
 ;; ;; FUNCTIONS
-
 ;; function to mark a whole line
 (defun select-current-line ()
    "Mark the current line"
@@ -126,74 +80,57 @@
   (find-file "~/.emacs.d/init.el"))
 
 
-
-;; ;; MELPA
+;; ;; MELPA and packages
 (require 'package)
 ;(add-to-list 'package-archives
 ;             '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize) ;; You might already have this line
 
+;; automatically load use-package to subsequently do loading automatically
+(if (not (package-installed-p 'use-package))
+    (progn
+      (package-refresh-contents)
+      (package-install 'use-package)))
+(require 'use-package)
+(setq-default use-package-always-ensure t)
 
 
-;; ;; OTHER CUSTOMIZATIONS
-;; general stuff
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq-default fill-column 100)
-(put 'downcase-region 'disabled nil)
+;; ;; OTHER CUSTOMIZATION MODES
 
-;; various key bindings
-(global-set-key (kbd "C-x a r") 'align-regexp)
-
-;; always ask the same way
-(fset 'yes-or-no-p 'y-or-n-p)
-
-;; define location of backups and save-places
-(setq 
-    backup-directory-alist `(("." . "~/.emacs.cache/emacssaves"))
-    kept-new-versions 1
-    kept-old-versions 1)
-
-(require 'saveplace)
-(setq-default save-place t)
-(setq save-place-file "~/.emacs.cache/saved-places")
-
-
-
-;; ;; MODE OPTIONS
+(use-package saveplace
+  :demand t
+  :config
+  (setq-default save-place t)
+  (setq save-place-file "~/.emacs.cache/saved-places"))
 
 ;; add rainbow delimiters to all programming modes
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(use-package rainbow-delimiters
+  :config
+  (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
 
-;; auto fill
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
-(add-hook 'tex-mode-hook 'turn-on-auto-fill)
-(remove-hook 'markdown-mode-hook 'turn-on-auto-fill)
+(use-package paren
+  :config
+  (set-face-background 'show-paren-match (face-background 'default))
+  (set-face-foreground 'show-paren-match "#def")
+  (set-face-attribute 'show-paren-match nil :weight 'extra-bold))
+(show-paren-mode t)
 
-
-;; color theme
-;(require 'color-theme)
+;; automatically set color theme depending on display/console
+(use-package color-theme-modern)
 (if (display-graphic-p)
-    (progn (load-theme 'bharadwaj t t)
-           (enable-theme 'bharadwaj))           
+      (progn (load-theme 'bharadwaj t t)
+             (enable-theme 'bharadwaj))
     (progn (load-theme 'tty-dark t t)
            (enable-theme 'tty-dark)))
 
-
-
-;; tabbar-mode
-(require 'tabbar)
-(tabbar-mode t)
-(global-set-key [M-left] 'tabbar-backward-tab)
-(global-set-key [M-right] 'tabbar-forward-tab)
-
-; change tab grouping: http://stackoverflow.com/a/3814313/1346276
+;; tabbar: show tabs at the top, automatically grouped
 (defun tabbar-buffer-groups ()
+  ;; http://stackoverflow.com/a/3814313/1346276
   "Return the list of group names the current buffer belongs to.
-This function is a custom function for tabbar-mode's tabbar-buffer-groups.
-This function group all buffers into 3 groups:
-Those Dired, those user buffer, and those emacs buffer.
+This function is a custom function for tabbar-mode's
+tabbar-buffer-groups.  This function group all buffers into 3
+groups: Those Dired, those user buffer, and those emacs buffer.
 Emacs buffer are those starting with “*”."
   (list
    (cond
@@ -202,142 +139,130 @@ Emacs buffer are those starting with “*”."
     ((eq major-mode 'dired-mode)
      "Dired")
     (t
-     "User Buffer")))) 
+     "User Buffer"))))
 
-(setq tabbar-buffer-groups-function 'tabbar-buffer-groups)
-
+(use-package tabbar
+  :bind (([M-left] . tabbar-backward-tab)
+         ([M-right] . tabbar-forward-tab))
+  :config
+  (setq tabbar-buffer-groups-function 'tabbar-buffer-groups))
+(tabbar-mode t)
 
 ;; markdown/pandoc
-(require 'markdown-mode)
-(require 'pandoc-mode)
-(add-hook 'markdown-mode-hook 'pandoc-mode)
-(add-hook 'markdown-mode-hook '(lambda ()
-                                (turn-off-auto-fill)))
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
-
-;; lisp mode
-(add-hook 'lisp-mode-hook '(lambda ()
-                             (local-set-key (kbd "RET") 'newline-and-indent))) 
-
+(use-package markdown-mode
+  :mode "\\.text\\'"
+  :mode "\\.md\\'"
+  :config
+  (require 'pandoc-mode)
+  (add-hook 'markdown-mode-hook 'pandoc-mode)
+  (add-hook 'markdown-mode-hook '(lambda ()
+                                   (turn-off-auto-fill)))
+  (remove-hook 'markdown-mode-hook 'turn-on-auto-fill))
 
 ;; haskell mode
-(require 'haskell-mode)
-(add-to-list 'auto-mode-alist '("\\.hs\\'" . haskell-mode))
-(defun my-haskell-mode-save-buffer ()
-  (interactive)
-  (save-buffer))
-(add-hook 'haskell-mode-hook (lambda ()
-  (define-key haskell-mode-map (kbd "C-c <right>") 'comment-region)
-  (define-key haskell-mode-map (kbd "C-c <left>") 'uncomment-region)
-  (define-key haskell-mode-map (kbd "C-x C-s") 'my-haskell-mode-save-buffer)
-  (local-set-key (kbd "M-q") 'align)))
-
-; alignment rules (https://github.com/haskell/haskell-mode/wiki/Indentation#aligning-code)
-(add-hook 'align-load-hook (lambda ()
-  (progn
-    (add-to-list 'align-rules-list
-                 '(haskell-types
-                   (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
-                   (modes quote (haskell-mode literate-haskell-mode))))
-    (add-to-list 'align-rules-list
-                 '(haskell-assignment
-                   (regexp . "\\(\\s-+\\)=\\s-+")
-                   (modes quote (haskell-mode literate-haskell-mode))))
-    (add-to-list 'align-rules-list
-                 '(haskell-arrows
-                   (regexp . "\\(\\s-+\\)\\(->\\|→\\)\\s-+")
-                   (modes quote (haskell-mode literate-haskell-mode))))
-    (add-to-list 'align-rules-list
-                 '(haskell-left-arrows
-                   (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+")
-                   (modes quote (haskell-mode literate-haskell-mode)))))))
-;;(autoload 'ghc-init "ghc" nil t)
-;;(add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode)))
-
-
-
+(use-package haskell-mode
+  :mode "\\.hs\\'"
+  :init
+  (defun my-haskell-mode-save-buffer ()
+    (interactive)
+    (save-buffer))
+  :bind (("M-q" . align)
+         :map haskell-mode-map
+         ("C-c <right>" . comment-region)
+         ("C-c <left>" . uncomment-region)
+         ("C-x C-s" . my-haskell-mode-save-buffer))
+  :config
+  ; alignment rules (https://github.com/haskell/haskell-mode/wiki/Indentation#aligning-code)
+  (add-hook 'align-load-hook (lambda ()
+                               (progn
+                                 (add-to-list 'align-rules-list
+                                              '(haskell-types
+                                                (regexp . "\\(\\s-+\\)\\(::\\|∷\\)\\s-+")
+                                                (modes quote (haskell-mode literate-haskell-mode))))
+                                 (add-to-list 'align-rules-list
+                                              '(haskell-assignment
+                                                (regexp . "\\(\\s-+\\)=\\s-+")
+                                                (modes quote (haskell-mode literate-haskell-mode))))
+                                 (add-to-list 'align-rules-list
+                                              '(haskell-arrows
+                                                (regexp . "\\(\\s-+\\)\\(->\\|→\\)\\s-+")
+                                                (modes quote (haskell-mode literate-haskell-mode))))
+                                 (add-to-list 'align-rules-list
+                                              '(haskell-left-arrows
+                                                (regexp . "\\(\\s-+\\)\\(<-\\|←\\)\\s-+")
+                                                (modes quote (haskell-mode literate-haskell-mode))))))))
+  ;;(autoload 'ghc-init "ghc" nil t)
+  ;;(add-hook 'haskell-mode-hook (lambda () (ghc-init) (flymake-mode))
 
 ;; cc mode
-(require 'cc-mode)
-(setq c-default-style "ellemtel"
-      c-basic-offset 2)
-(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
-(define-key c-mode-base-map (kbd "C-c M-c") 'uncomment-region)
-(add-to-list 'auto-mode-alist '("\\.cpp\\'" . c++-mode))
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-
-; indentation for c++-code
-(setq c-echo-syntactic-information-p t)
-
-(add-to-list 'c-offsets-alist
-  '(access-label . +)
-  '(inclass . +)
-  ;'(case-label . +)
-)
-
-(c-set-offset 'access-label -2)
-(c-set-offset 'inclass 4)
-(c-set-offset 'topmost-intro 0)
-(c-set-offset 'topmost-intro-cont 0)
-(c-set-offset 'inline-open 0)
-(c-set-offset 'case-label 2)
-(c-set-offset 'cpp-macro 0)
-(c-set-offset 'friend -2)
-(c-set-offset 'innamespace 2)
-(c-set-offset 'comment-intro 0)
-
-
-;; dired
-(put 'dired-find-alternate-file 'disabled nil)
-(add-hook 'dired-mode-hook
- (lambda ()
-  (define-key dired-mode-map (kbd "^")
-    (lambda () (interactive) (find-alternate-file "..")))
-  ; was dired-up-directory
- )
-)
-
+(use-package cc-mode
+  :bind (:map c-mode-base-map
+              ("RET" . newline-and-indent)
+              ("C-c M-c" . uncomment-region))
+  :mode ("\\.cpp\\'" . c++-mode)
+  :mode ("\\.h\\'" . c++-mode)
+  :config
+  (setq c-default-style "ellemtel"
+        c-basic-offset 2)
+  ;; indentation for c++-code
+  (setq c-echo-syntactic-information-p t) 
+  (add-to-list 'c-offsets-alist
+               '(access-label . +)
+               '(inclass . +)
+               ;;'(case-label . +)
+               )
+  (c-set-offset 'access-label -2)
+  (c-set-offset 'inclass 4)
+  (c-set-offset 'topmost-intro 0)
+  (c-set-offset 'topmost-intro-cont 0)
+  (c-set-offset 'inline-open 0)
+  (c-set-offset 'case-label 2)
+  (c-set-offset 'cpp-macro 0)
+  (c-set-offset 'friend -2)
+  (c-set-offset 'innamespace 2)
+  (c-set-offset 'comment-intro 0))
 
 ;; octave mode
-(add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
-
+(use-package octave
+  :mode "\\.m$")
 
 ;; ess-mode
-(require 'ess-site)
-
+(use-package ess)
 
 ;; scala-mode2
-(require 'scala-mode2)
-(add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
-
+(use-package scala-mode2
+  :mode ("\\.scala$" . scala-mode))
 
 ;; web-mode
-(require 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.xhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
-
+(use-package web-mode
+  :mode ("\\.html?\\'" . web-mode)
+  :mode ("\\.xhtml\\'" . web-mode)
+  :mode ("\\.css\\'" . web-mode)
+  :mode ("\\.php$" . web-mode))
 
 ;; darkroom-mode
-(require 'darkroom)
-
+(use-package darkroom
+  :disabled t)
 
 ;; multiple-cursors
-(require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C-S-c a") 'mc/edit-beginnings-of-lines)
-(global-set-key (kbd "C-S-c e") 'mc/edit-ends-of-lines)
-(global-set-key (kbd "C-S-c %") 'mc/mark-all-in-region)
-
+(use-package multiple-cursors
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C-S-c a" . mc/edit-beginnings-of-lines)
+         ("C-S-c e" . mc/edit-ends-of-lines)
+         ("C-S-c %" . mc/mark-all-in-region)))
 
 ;; auctex
-(add-to-list 'auto-mode-alist 
-             '("\\.sg$" . (lambda ()
-                            (latex-mode)
-                            (electric-indent-mode 0))))
+(use-package tex
+  :ensure auctex
+  :config
+  (add-to-list 'auto-mode-alist 
+               '("\\.sg$" . (lambda ()
+                              (latex-mode)
+                              (electric-indent-mode 0)))))
 
 ;; JavaScript
-(setq js-indent-level 2)
-(setq js-switch-indent-offset 2)
+(use-package js-mode
+  :ensure js2-mode
+  :config
+  (setq js-indent-level 2)
+  (setq js-switch-indent-offset 2))
