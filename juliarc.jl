8d005715a6e772889c58cc6a5c8c1c1bbf8258ff
Philipp Gabler <phipsgabler@users.noreply.github.com>
2017-10-15 10:23:12 +0200

    Update juliarc: add Revise, guard imports

--- juliarc.jl
+++ juliarc.jl
@@ -1,1 +1,16 @@
-using OhMyREPL
+let pkgs = Pkg.installed()
+    if haskey(pkgs, "OhMyREPL")
+        using OhMyREPL
+    else
+        warn("'OhMyREPL' is not installed")
+    end
+
+    if haskey(pkgs, "Revise")
+        @schedule begin
+            sleep(0.1)
+            @eval using Revise
+        end
+    else
+        warn("'Revise' is not installed")
+    end
+end
