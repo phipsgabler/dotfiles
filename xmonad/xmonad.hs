e7d319a657d4dc8c5f28b4f05f48177635125eef
Philipp Gabler <phipsgabler@users.noreply.github.com>
2018-01-14 16:57:52 +0200

    Xmonad: remove autostarts

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -13,12 +13,13 @@ main = xmonad $ xfceConfig
   { modMask = mod4Mask
   , terminal = "xfce4-terminal"
   , workspaces = map show [1 .. 9 :: Int]
-  , startupHook = do
-      spawnOn "1" "firefox"
-      spawnOn "2" "xfce4-terminal"
-      spawnOn "2" "emacs"
-      spawnOn "3" "thunar"
-      spawnOn "3" "keepassx" }
+  -- , startupHook = do
+  --     spawnOn "1" "firefox"
+  --     spawnOn "2" "xfce4-terminal"
+  --     spawnOn "2" "emacs"
+  --     spawnOn "3" "thunar"
+  --     spawnOn "3" "keepassx"
+  }
   `additionalKeys` myKeys
 
 myKeys = [((mod4Mask, xK_p), spawn "exe=$(dmenu_path | dmenu) && eval \"exec $exe\"")]
